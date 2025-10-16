f791ed6ded8e15224f1aedd7500e1818a30c2e6b
JoseAv <jm.aranav@gmail.com>
2025-10-15 23:52:56 -0600

    Terminado tablas con paginacion, filtros, columnas, filtros personzalidos

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ColumnDef } from "@tanstack/react-table"
+import { ColumnDef, FilterFn, Row } from "@tanstack/react-table"
 import { Payment } from "./data/data";
 import { Badge } from "@/components/ui/badge";
 import {
@@ -12,8 +12,9 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button";
-import { MoreHorizontal } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 import { toast } from "sonner";
+import { Checkbox } from "@/components/ui/checkbox"
 
 const showBadget: any = {
     ["pending"]: "default",
@@ -23,67 +24,138 @@ const showBadget: any = {
 }
 
 
+const customFilterFn: FilterFn<Payment> = (rows: Row<Payment>, columnId: string, filterValue: number | string,) => {
+    const data = rows.original
+    const separte = String(filterValue).split(' ')
+    // for (let letter of separte) {
+    //     if (data.email.includes(String(letter)) || data.status.includes(String(letter)) || data.clientName.includes(String(letter))) {
+    //     } else {
+    //         return false
+    //     }
+    // }
+
+    const rowValues = `${data.email} ${data.clientName} ${data.status}`.toLowerCase()
+
+    // aqui se usa every porque every evalua que todo se cumpla es decir 'to, love' -> part sacara to,love y esto va a devolver true solo si las 2 palabras existen ya se
+    // en data.email o data.client o data.status haciendo esto un filtro muy inteligente porque compara con los 3 campos y a la ves lo hace ligero
+    return separte.every((part) => rowValues.includes(part))
+
+}
 
 
-export const columns: ColumnDef<Payment>[] = [
-    {
-        accessorKey: "clientName",
-        header: "Client Name",
-    },
-    {
-        accessorKey: "status",
-        header: "Status",
-        cell: ({ row }) => {
-            const values: string = row.getValue("status")
-            return <Badge variant={showBadget[values]}>{`${values}`}</Badge>
-        }
-    },
-    {
-        accessorKey: "amount",
-        header: "Amount",
-        cell: ({ row }) => {
-            console.log(row)
-            const amount = parseFloat(row.getValue("amount"))
-            const formatted = new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-            }).format(amount)
-            return <div className="font-medium">{formatted}</div>
-        },
-    },
-    {
-        accessorKey: "email",
-        header: "Email",
-    },
-    {
-        id: "actions",
-        cell: ({ row }) => {
-            const payment = row.original
-            return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                            onClick={() => {
-                                navigator.clipboard.writeText(payment.id)
-                                toast('Copy clipboard', { position: 'top-right' })
-                            }}
-                        >
-                            Copy payment ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View customer</DropdownMenuItem>
-                        <DropdownMenuItem>View payment details</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            )
+export const columns: ColumnDef<Payment>[] = [{
+    id: "select",
+    header: ({ table }) => (
+        <Checkbox
+            checked={
+                table.getIsAllPageRowsSelected() ||
+                (table.getIsSomePageRowsSelected() && "indeterminate")
+            }
+            onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+            aria-label="Select all"
+        />
+    ),
+    cell: ({ row }) => (
+        <Checkbox
+            checked={row.getIsSelected()}
+            onCheckedChange={(value) => row.toggleSelected(!!value)}
+            aria-label="Select row"
+        />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+},
+{
+    accessorKey: "clientName",
+    header: "Client Name",
+    filterFn: customFilterFn,
 
-        }
+},
+{
+    accessorKey: "status",
+    filterFn: customFilterFn,
+    header: ({ column }) => {
+        return (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Status
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        )
     },
+    cell: ({ row }) => {
+        const values: string = row.getValue("status")
+        return <Badge variant={showBadget[values]}>{`${values}`}</Badge>
+    }
+},
+{
+    accessorKey: "amount",
+    header: ({ column }) => {
+        return (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Amaunt
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        )
+    },
+    cell: ({ row }) => {
+        const amount = parseFloat(row.getValue("amount"))
+        const formatted = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(amount)
+        return <div className="font-medium">{formatted}</div>
+    },
+},
+{
+    accessorKey: "email",
+    filterFn: customFilterFn,
+    header: ({ column }) => {
+        return (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Email
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        )
+    },
+},
+{
+    id: "actions",
+    cell: ({ row }) => {
+        const payment = row.original
+        return (
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                        <span className="sr-only">Open menu</span>
+                        <MoreHorizontal className="h-4 w-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem
+                        onClick={() => {
+                            navigator.clipboard.writeText(payment.id)
+                            toast('Copy clipboard', { position: 'top-right' })
+                        }}
+                    >
+                        Copy payment ID
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>View customer</DropdownMenuItem>
+                    <DropdownMenuItem>View payment details</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        )
+
+    }
+},
 ]
--- src/app/dashboard/data-table/data-table.tsx
+++ src/app/dashboard/data-table/data-table.tsx
@@ -1,11 +1,18 @@
 "use client"
+import { useState } from "react"
 
 import {
     ColumnDef,
     flexRender,
     getCoreRowModel,
     useReactTable,
+    SortingState,
+    getSortedRowModel,
     getPaginationRowModel,
+    ColumnFiltersState,
+    getFilteredRowModel,
+    VisibilityState,
+    Row,
 } from "@tanstack/react-table"
 
 import {
@@ -16,7 +23,27 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { Payment } from "./data/data"
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -27,15 +54,124 @@ export function DataTable<TData, TValue>({
     columns,
     data,
 }: DataTableProps<TData, TValue>) {
+
+    const [sorting, setSorting] = useState<SortingState>([])
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
+        []
+    )
+    const [status, setStatus] = useState<string>('')
+    const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
+    const [rowSelection, setRowSelection] = useState({})
+
+
     const table = useReactTable({
         data,
         columns,
+        onSortingChange: setSorting,
+        // globalFilterFn: customFilterFn,
+        onColumnFiltersChange: setColumnFilters,
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        onRowSelectionChange: setRowSelection,
+        onColumnVisibilityChange: setColumnVisibility,
+
+        state: {
+            sorting,
+            columnFilters,
+            columnVisibility,
+            rowSelection,
+
+        },
     })
 
+    const changeStatus = ({ status }: { status: string }) => {
+        const newStatus = status !== 'all' ? status : ""
+        table.getColumn("status")?.setFilterValue(newStatus)
+
+    }
+
+    const deleteRows = () => {
+        const ids = table.getSelectedRowModel().rows.map((ele) => (ele.original as Payment).clientName)
+        console.log(ids)
+    }
+
+
     return (
-        <div className="overflow-hidden rounded-md border">
+        <div className="overflow-hidden rounded-md border ">
+            <Button variant='destructive' onClick={deleteRows}>Eliminar</Button>
+
+            <div className="flex items-center py-4 justify-between p-12">
+                <Input
+                    placeholder="Filter emails..."
+                    value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) => {
+                        setStatus('all')
+                        table.getColumn("status")?.setFilterValue("")
+                        table.getColumn("email")?.setFilterValue(event.target.value)
+                    }
+
+                    }
+                    className="max-w-sm"
+                />
+                <p>{status}</p>
+                <Select onValueChange={(value) => {
+                    setStatus(value)
+                    changeStatus({ status: value })
+                }} value={status}>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Select a fruit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Status</SelectLabel>
+                            <SelectItem value='all'>ALL</SelectItem>
+                            <SelectItem value='pending'>pending</SelectItem>
+                            <SelectItem value='processing'>processing</SelectItem>
+                            <SelectItem value='success'>success</SelectItem>
+                            <SelectItem value='failed'>failed</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+
+                <div>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" className="ml-auto">
+                                Columns
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            {table
+                                .getAllColumns()
+                                .filter(
+                                    (column) => column.getCanHide()
+                                )
+                                .map((column) => {
+                                    return (
+                                        <DropdownMenuCheckboxItem
+                                            key={column.id}
+                                            className="capitalize"
+                                            checked={column.getIsVisible()}
+                                            onCheckedChange={(value) =>
+                                                column.toggleVisibility(!!value)
+                                            }
+                                        >
+                                            {column.id}
+                                        </DropdownMenuCheckboxItem>
+                                    )
+                                })}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+
+
+            </div>
+
+
+
+
             <Table>
                 <TableHeader>
                     {table.getHeaderGroups().map((headerGroup) => (
@@ -97,6 +233,31 @@ export function DataTable<TData, TValue>({
                     Next
                 </Button>
             </div>
+
+            <div className="text-muted-foreground flex-1 text-sm">
+                {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                {table.getFilteredRowModel().rows.length} row(s) selected.
+            </div>
+            <Select
+                onValueChange={(value) => {
+                    table.setPageSize(+value)
+                }}
+            >
+                <SelectTrigger>
+                    <SelectValue placeholder='10 rows'></SelectValue>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Rows per page</SelectLabel>
+                            <SelectItem value="10">10</SelectItem>
+                            <SelectItem value="20">20</SelectItem>
+                            <SelectItem value="30">30</SelectItem>
+
+
+                        </SelectGroup>
+                    </SelectContent>
+                </SelectTrigger>
+            </Select>
+
 
         </div>
     )
